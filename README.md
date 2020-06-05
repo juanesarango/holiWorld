4457ab695600841346752fa16d2f10e2f0f13d6d
Juan Esteban Arango <juanes.ao@gmail.com>
2020-06-05 14:20:16 -0400

    🔧 add configs and doc

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+![](https://img.shields.io/github/v/tag/juanesarango/HoliWorld?style=flat-square)
+![](https://img.shields.io/visual-studio-marketplace/d/juanesarango.holiworld?style=flat-square)
+![](https://img.shields.io/github/stars/juanesarango/HoliWorld?style=flat-square)
+
 # Holi World ✌🏼
 
 This is a huge must-have VS Code extension for everyone. It prompts a motivation message on startup on your editor, saying:
 
     Holi! ✌🏼, remember you're awesome ✨
+
+## Run with VS Command
+
+Enter VS Code Command Pallete `ctrl+shift+p` (`cmd+shift+p` on Mac), and type:
+`Holi World: Say Holi!` command.
+
+## How it looks
+
+![Screenshot](https://user-images.githubusercontent.com/7906289/83904936-cabdec00-a72e-11ea-968a-4cda7c3b1160.png)
--- extension.js
+++ extension.js
@@ -16,17 +16,18 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    'holiworld.sayHoli',
-    function () {
-      // The code you place here will be executed every time your command is executed
+  var holi = function () {
+    // The code you place here will be executed every time your command is executed
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Holi! ✌🏼, remember you're awesome ✨!"
-      )
-    }
-  )
+    // Display a message box to the user
+    vscode.window.showInformationMessage(
+      "Holi! ✌🏼, remember you're awesome ✨!"
+    )
+  }
+
+  holi()
+
+  let disposable = vscode.commands.registerCommand('holiworld.sayHoli', holi)
 
   context.subscriptions.push(disposable)
 }
